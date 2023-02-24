f6f343c0fe69c2f6ccf49e3b9c68efc0d3a3990c
song <55rsearchco@gmail.com>
2023-02-24 17:03:27 +0900

    투두 아이템 저장하기

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,19 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 import styles from './TodoList.module.css';
 
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState([
-    { id: '123', text: '장보기', status: 'active' },
-    { id: '124', text: '공부하기', status: 'active' },
-  ]);
+  const [todos, setTodos] = useState(readTodosFromLocalStorage);
 
   const handleAdd = todo => setTodos([...todos, todo]);
+
   const handleUpdate = updated =>
     setTodos(todos.map(t => (t.id === updated.id ? updated : t)));
+
   const handleDelete = deleted =>
     setTodos(todos.filter(t => t.id !== deleted.id));
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   const filtered = getFilteredItems(todos, filter);
   return (
@@ -33,9 +36,14 @@ export default function TodoList({ filter }) {
   );
 }
 
+function readTodosFromLocalStorage() {
+  const todos = localStorage.getItem('todos');
+  return todos ? JSON.parse(todos) : [];
+}
+
 function getFilteredItems(todos, filter) {
   if (filter === 'all') {
     return todos;
   }
-  return todos.filter(todo => todo.status === filter);
+  return todos.filter(todos => todos.status === filter);
 }
